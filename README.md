db9ee5a00fa8167badfa03175fc0d1ab5b7c2ad6
yunseop-dev <c.o.d.e@kakao.com>
2020-05-23 11:49:20 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# vue-storybook-tutorial
+
+See the accompanying blog post: [Using Storybook with Vue single file components
+](https://dev.to/josephuspaye/using-storybook-with-vue-single-file-components-2od).
+
+## Project setup
+
+```
+yarn install
+```
+
+### Run Storybook
+
+```
+yarn storybook
+```
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Button>Normal Button</Button> <Button color="primary">Primary Button</Button>
+  </div>
+</template>
+
+<script>
+import Button from './components/Button.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
